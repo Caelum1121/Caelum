a1ab60d1160f5f5cce544f715a05b8ed8586e6cf
Caelum1121 <s4073761@rmit.edu.vn>
2024-10-25 12:45:37 +0700

    Update Model

--- script.js
+++ script.js
@@ -4,3 +4,21 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+function openModal(modalId) {
+  document.getElementById(modalId).style.display = 'block';
+}
+
+function closeModal(modalId) {
+  document.getElementById(modalId).style.display = 'none';
+}
+
+// Close modal if user clicks outside content
+window.onclick = function(event) {
+  const modals = document.querySelectorAll('.custom-modal');
+  modals.forEach(modal => {
+    if (event.target === modal) {
+      modal.style.display = 'none';
+    }
+  });
+}
